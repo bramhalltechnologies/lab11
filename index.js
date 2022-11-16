5307a536377312d8e50babdb2b6357da67e26d7f
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 17:59:37 -0500

    post adjustment

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  path.post('/rides.json', function(request, response) {
+  .post('/rides.json', function(request, response) {
     response.send("[]");
-  });
+  }) 
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
