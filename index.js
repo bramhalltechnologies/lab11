96be555f5eea15eaf985f4a30c5ce04c1218a832
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 20:06:03 -0500

    test

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/rides', function(request, response) {
-    var username = request.body.username;
-    var lat = request.body.lat;
-    var lon = request.body.lng;
-    if(username ==  "" || lat == "" || lng == ""){
-      response.send("bad");
-    } else {
+    // var username = request.body.username;
+    // var lat = request.body.lat;
+    // var lon = request.body.lng;
+    // if(username ==  "" || lat == "" || lng == ""){
+    //   response.send("bad");
+    // } else {
       response.send("good");
-    }
+    // }
   }) 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
