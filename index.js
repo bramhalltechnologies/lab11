499d1ba8e1406336f01e3d705033cf9d931c77aa
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 19:39:06 -0500

    test

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const path = require('path')
+var bodyParser = require('body-parser');
+var validator = require('validator');
+
+var app = express();
+
+// See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
+app.use(bodyParser.json());
+
+// See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP post parameters
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/rides', function(request, response) {
-    response.send(String(request.body));
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.post('/rides', function(request, response) {
+  response.send(String(request.body));
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
