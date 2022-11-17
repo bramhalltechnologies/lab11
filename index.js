1e03e030480739f8654b9e1d6d858d765d172473
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 22:10:14 -0500

    testing pg connect

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+var postgres = require('pg');
+
+var connection = postgres.createConnection({
+  host     : "sub-bum.c1tjeabvxzjt.us-east-1.rds.amazonaws.com",
+  user     : "subbum",
+  password : "subbumrocks!",
+  port     : "5432"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -29,4 +48,5 @@ app
       response.send("good");
     }
   }) 
+  .post(/)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
