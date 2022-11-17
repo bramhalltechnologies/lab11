2a6c593f53bd265ba784a142731ece72ab42b107
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 20:19:09 -0500

    test app

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
