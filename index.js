6305359f36bb8067003190fe043428d952bf96a7
Harrison Bramhall <harry.bramhall@gmail.com>
2022-11-16 21:19:00 -0500

    working, but not with notubder test1

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app
     var username = request.body.username;
     var lat = request.body.lat;
     var lon = request.body.lng;
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-headers", "X-Requested-With")
     if(username.length == 0 || lat.length ==  0 || lon.length == 0){
       // throw new Error('Whoops, something is wrong with your data!');
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-headers", "X-Requested-With")
       response.json({"error":"Whoops, something is wrong with your data!"});
       response.send("bad");
       // response.send({"error":"Whoops, something is wrong with your data!"});
